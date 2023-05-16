51863d01c6e93d36fe3788e21f55bf0daf7e9769
kemboi590 <bkemboi590@gmail.com>
2023-05-16 15:50:00 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import React from 'react'
+import './App.css'
+import Header from '../../components/header/Header'
+import Blogs from '../../components/myblogs/Blogs'
+import Footer from '../../components/footer/Footer'
+
+
+
+
+function App() {
+
+  return (
+    <>
+      <Header />  
+      <Blogs />
+      <Footer/>
+
+
+
+
+    </>
+  )
+}
+
+
+
+export default App
